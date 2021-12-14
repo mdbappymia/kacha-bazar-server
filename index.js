6d87322e461c6378f543e62702e6896aac234212
mdbappymia <mbm.21.02.16@gmail.com>
2021-12-15 00:14:52 +0600

    initial

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// connection uri
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.iuevi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Database connected");
+
+    const database = client.db("kacha_bazer");
+    const productCollection = database.collection("products");
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      console.log(product);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
